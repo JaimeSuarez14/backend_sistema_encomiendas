6bd10c565e5d171fedd7b090c7dcf7abe7bd1717
Jaime Suarez <jaisua8@gmail.com>
2026-04-14 22:07:43 -0500

    creacion del proyecto backend en spring boot

---
+++ src/main/java/utp/edu/sistema_encomiendas/SistemaEncomiendasApplication.java
@@ -0,0 +1,13 @@
+package utp.edu.sistema_encomiendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEncomiendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEncomiendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utp/edu/sistema_encomiendas/SistemaEncomiendasApplicationTests.java
@@ -0,0 +1,13 @@
+package utp.edu.sistema_encomiendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEncomiendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
